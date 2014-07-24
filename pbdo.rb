827bec7e769539f1cb7ae46dd64ac1ea1808134b
Ashkan Kiani <ashkan.k.kiani@gmail.com>
2014-07-24 14:36:17 -0700

    pbdo: 0.1.2

--- pbdo.rb
+++ pbdo.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Pbdo < Formula
   homepage 'https://github.com/norcalli/scripts'
   url 'https://github.com/norcalli/scripts', :using => :git
-  version '0.1.1'
+  version '0.1.2'
 
   def install
     bin.install "pbdo"
